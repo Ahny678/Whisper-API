bb9f92df155b854c9a74371e27c8375e48f4be41
Tiffany Nebo <nebotiffany@gmail.com>
2025-04-06 12:13:33 +0100

    Created user model

---
+++ migrations/20250406111011-create-users-table.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isAuthor: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+var bcrypt = require("bcryptjs");
+
+class User extends Model {
+  //Compares a plain password (e.g., entered during login) with a hashed password from the database.
+  async verify(password) {
+    return await bcrypt.compare(password, this.password);
+  }
+}
+
+User.init(
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        isUUID: 4,
+      },
+    },
+
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        const hashedPassword = bcrypt.hashSync(value, 10);
+        this.setDataValue("password", hashedPassword);
+      },
+    },
+    isAuthor: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    // Other model options go here
+    sequelize,
+    modelName: "User",
+  }
+);
+
+module.exports = User;
