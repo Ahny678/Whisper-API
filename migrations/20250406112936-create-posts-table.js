ef063dd954bf5b914af6b3c54b2946c13d154407
Tiffany Nebo <nebotiffany@gmail.com>
2025-04-06 12:39:34 +0100

    Created Post model

---
+++ migrations/20250406112936-create-posts-table.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Posts", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        validate: {
+          isUUID: 4,
+        },
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isPublished: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      publishedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "SET NULL",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Posts");
+  },
+};
---
+++ models/post.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+const Post = require("./user");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        isUUID: 4,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isPublished: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    publishedAt: {
+      type: DataTypes.DATE,
+      allowNull: true, // Starts as null until published
+    },
+  },
+  {
+    // Other model options go here
+    sequelize,
+    modelName: "post",
+  }
+);
+
+//one to many -> user -- posts
+Post.belongsTo(User, {
+  foreignKey: {
+    name: "userId",
+    allowNull: false,
+    type: DataTypes.UUID,
+  },
+});
+
+User.hasMany(Post, {
+  foreignKey: "userId",
+});
+
+module.exports = Post;
