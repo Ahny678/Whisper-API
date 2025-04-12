9c40a44a95f93a5001539b97c62af4cec3041a79
Tiffany Nebo <nebotiffany@gmail.com>
2025-04-12 20:00:37 +0100

    Populated db with more seeders

--- seeders/20250406140324-users.js
+++ seeders/20250406140324-users.js
@@ -33,6 +33,15 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      ...Array.from({ length: 7 }, (_, i) => ({
+        id: uuidv4(),
+        username: `user${i + 3}`,
+        email: `user${i + 3}@dev.com`,
+        password: bcrypt.hashSync("pass123", 10),
+        isAuthor: i % 2 === 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })),
     ]);
   },
 
